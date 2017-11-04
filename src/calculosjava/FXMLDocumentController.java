da859e7ee6fa7691b3456a8188f390891210bf3a
Adm <Adm@Adm>
2017-11-04 20:13:46 -0200

    segunda versão com system out

--- src/calculosjava/FXMLDocumentController.java
+++ src/calculosjava/FXMLDocumentController.java
@@ -25,6 +25,7 @@ public class FXMLDocumentController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        System.out.println("Olá Mundo GIT!!!");
     }
     
     @Override
